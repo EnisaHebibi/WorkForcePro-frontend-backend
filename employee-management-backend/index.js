aad4b79d7d3166eaace5212493b9594795432b0f
Enisa Hebibi <isahebibi@gmail.com>
2025-12-01 03:04:42 +0100

    Endpoint -create new employee by department Id and update existing employee by id:  // Endpoint -get all employees by department id   //Endpoint - get employee by id

--- employee-management-backend/index.js
+++ employee-management-backend/index.js
@@ -7,8 +7,10 @@ server.use(jsonServer.bodyParser);
 server.use(middlewares);
 
 const departmentsRoute = require("./routes/departments");
+const employeesRoute = require("./routes/employees");
 
 departmentsRoute(server);
+employeesRoute(server);
 
 server.listen(jsonServerPort, () => {
   console.log(
