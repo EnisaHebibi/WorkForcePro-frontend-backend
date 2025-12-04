82686553298e7ed053d68197dfb2949f768933e9
Enisa Hebibi <isahebibi@gmail.com>
2025-12-04 23:29:36 +0100

    implementing express in WorkForceePro

---
+++ employee-management-express/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+//import routes
+
+const app = express();
+
+app.use(cors({ origin: process.env.DOMAIN }));
+app.use(express.json());
+
+//use routes
+
+const PORT = process.env.PORT || 8095;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: http://localhost:${PORT}`);
+});
