fff14a984bbd89035578530014a4762ec58b7fd9
Enisa Hebibi <isahebibi@gmail.com>
2025-12-25 05:08:10 +0100

    implemented delete button, with all functionalities

---
+++ employee-management-frontend/src/components/shared/dashboard/overview/department/DeleteButton.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+import { toast } from "sonner";
+
+const DeleteButton = ({ departmentName, departmentId, onDelete }) => {
+  const [showDialog, setShowDialog] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleDeleteConfirm = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/departments/${departmentId}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")} `,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to delete department");
+      }
+      toast.success("Succes", {
+        description: `Department ${departmentName} has been deleted successfully`,
+      });
+
+      onDelete();
+      setShowDialog(false);
+    } catch (error) {
+      toast.error("Error", {
+        description: error.message || "An unexpected error ocurred!",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      <Button
+        variant="destructive"
+        size="icon"
+        onClick={() => setShowDialog(true)}
+        className="cursor-pointer"
+      >
+        <Trash />
+      </Button>
+
+      <Dialog open={showDialog} onOpenChange={setShowDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Delete Department</DialogTitle>
+            <DialogDescription>
+              Are you sure you want to delete <strong>{departmentName}?</strong>
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button variant="outline" onClick={() => setShowDialog(false)}>
+              Cancel
+            </Button>
+            <Button
+              variant="destructive"
+              onClick={handleDeleteConfirm}
+              disabled={loading}
+            >
+              {loading ? "Deleting..." : "Delete"}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default DeleteButton;
