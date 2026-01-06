c5660c06aabd038b86df33e63909af0733104fe1
Enisa Hebibi <isahebibi@gmail.com>
2026-01-06 22:10:21 +0100

    fetch Employees

--- employee-management-frontend/src/store/employeesSlice.js
+++ employee-management-frontend/src/store/employeesSlice.js
@@ -1,1 +1,26 @@
-//employees slice
+import { createSlice, createAsyncThunk } from "@/reduxjs/toolkit";
+
+export const fetchEmployees = createAsyncThunk(
+  "employees/fetchEmployees",
+  async (_, { rejectedWithValue }) => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/employees/all-users`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch employees");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return rejectedWithValue(error.message);
+    }
+  }
+);
