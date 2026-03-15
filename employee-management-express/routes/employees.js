8d0211a2c09d6564dea23cbf10410f923f7fe7fc
Enisa Hebibi <isahebibi@gmail.com>
2026-03-15 08:42:14 +0100

    puting limit for data on a page

--- employee-management-express/routes/employees.js
+++ employee-management-express/routes/employees.js
@@ -18,7 +18,7 @@ router.get("/all-users", authMiddleware, async (req, res) => {
           FROM users u
           LEFT JOIN users_departments ud ON u.id = ud.user_id
           LEFT JOIN departments d ON d.id = ud.department_id
-          ORDER BY u.id ASC;`;
+          ORDER BY u.id ASC LIMIT 10;`;
 
     const result = await pool.query(query);
 
