7bc13d10b41eeecdb561594b52113085d328ecc6
Enisa Hebibi <isahebibi@gmail.com>
2026-03-15 01:14:00 +0100

    new function for users

---
+++ employee-management-frontend/src/components/shared/dashboard/employees/EditEmployeeDepartment.jsx
@@ -0,0 +1,96 @@
+import {
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Toaster } from "@/components/ui/sonner";
+import React, { useEffect } from "react";
+import { toast } from "sonner";
+
+const EditEmployeeDepartment = (
+  userId,
+  currentDepartmentName,
+  departments,
+  onDepartmentUpdated,
+) => {
+  const [selectedDepartment, setSelectedDepartment] = useState(
+    currentDepartmentName,
+  );
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (currentDepartmentName) {
+      setSelectedDepartment(currentDepartmentName);
+    } else {
+      setSelectedDepartment("No department selected!");
+    }
+  }, [currentDepartmentName]);
+
+  const handleDepartmentChange = async (value) => {
+    setLoading(true);
+
+    try {
+      const department = departments.find((dept) => dept.name === value);
+      const newDepartmentId = department ? department.id : null;
+
+      if (!newDepartmentId) {
+        throw new Error("Invalid department.");
+      }
+
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/employees/update-department`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify({
+            department_id: newDepartmentId,
+            user_id: userId,
+          }),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to update department.");
+      }
+      setSelectedDepartment(value);
+      onDepartmentUpdated(userId, newDepartmentId);
+      const data = await response.json();
+
+      toast.success("Success", { description: data.message });
+    } catch (error) {
+      toast.error("Error", {
+        description: error.message,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Select
+      value={selectedDepartment}
+      onValueChange={handleDepartmentChange}
+      disabled={loading}
+    >
+      <SelectTrigger className="w-fit h-5 font-medium">
+        <SelectValue placeholder="Select deprtment" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem disabled="true" value="Unasigned">
+          Unasigned
+        </SelectItem>
+        {departments.map((department) => (
+          <SelectItem key={department.id} value={department.name}>
+            {department.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default EditEmployeeDepartment;
